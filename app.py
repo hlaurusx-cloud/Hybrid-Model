be00fda0bf46890d6d48d1d37d5c199a75325ba8
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-12-01 19:37:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ elif st.session_state.step == 2:
                 "🎯 타겟 변수 (Y) 선택",
                 options = all_cols,
                 index = default_index,
-                help = '예측하고자 하는 목표 변수입니다."
+                help = "예측하고자 하는 목표 변수입니다."
             )
         
 
